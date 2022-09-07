3eaa01e926e20bf14d2b6d84dd5c53b4b5eb78bf
Ayush Srivastav <61233311+ayushsrivastav0811@users.noreply.github.com>
2022-09-08 00:47:26 +0530

    Time: 11 ms (88.62%), Space: 10.5 MB (67.27%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,22 +1,39 @@
-class Solution {
-    int dfs(int index,int prev,vector<int>&nums,vector<vector<int>>&dp){
-//         base case
-        if(index==nums.size())
+class Solution
+{
+    int dfs(int index, int prev, vector<int> &nums, vector<vector< int>> &dp)
+    {
+       	//         base case
+        if (index == nums.size())
             return 0;
-        if(dp[index][prev+1]!=-1)
-            return dp[index][prev+1];
-        int not_take=0+dfs(index+1,prev,nums,dp);
-        
-    int take=INT_MIN;
-        if(prev==-1 || nums[index]>nums[prev])
-             take=1+dfs(index+1,index,nums,dp);
-        
-        return dp[index][prev+1]= max(not_take,take);
+        if (dp[index][prev + 1] != -1)
+            return dp[index][prev + 1];
+        int not_take = 0 + dfs(index + 1, prev, nums, dp);
+
+        int take = INT_MIN;
+        if (prev == -1 || nums[index] > nums[prev])
+            take = 1 + dfs(index + 1, index, nums, dp);
+
+        return dp[index][prev + 1] = max(not_take, take);
     }
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
-        int prev=-1;
-        return dfs(0,prev,nums,dp);
-    }
+    public:
+        int lengthOfLIS(vector<int> &nums)
+        {
+            vector<int> arr;
+            arr.push_back(nums[0]);
+            int len = 1;
+            for (int i = 1; i < nums.size(); i++)
+            {
+                if (nums[i] > arr.back())
+                {
+                    arr.push_back(nums[i]);
+                    len++;
+                }
+                else
+                {
+                    auto ind = lower_bound(arr.begin(), arr.end(), nums[i]);
+                    *ind = nums[i];
+                }
+            }
+            return len;
+        }
 };
